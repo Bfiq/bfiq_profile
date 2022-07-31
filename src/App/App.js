d696e78f93ed7a689678bdc61cf6e81d768f06ba
Brian <brianfiquitiva12@gmail.com>
2022-07-31 14:58:09 -0500

    adding header with the navigation bar

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+//import './App.css';
 import './AppUI'
 import React from 'react';
 import { AppUI } from './AppUI';
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import './Header.css';
 import React from 'react';
+import {NavigationBar} from '../Navbar/NavigationBar';
 
 function Header() {
   return (
     <React.Fragment>
       <header>
+        <NavigationBar/>
         <p>Hola, soy Brian Fiquitiva, desarrollador web</p>
       </header>
     </React.Fragment>
